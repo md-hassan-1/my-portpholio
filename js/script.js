c02e0e6c20c4da14e886c0d6f8d8ab00fb826ab7
md-hassan-1 <hassanansari8533@gmail.com>
2024-02-15 22:16:11 +0530

    fourth commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let typed =  new Typed(".typing",{
+    Strings:[ ",","web designer","web developer","graphic designer","youtuber"],
+    typeSpeed:100,
+    Backspeed:60,
+    loop:true
+})
--- js/style-switcher.js
+++ js/style-switcher.js
@@ -28,9 +28,29 @@ const styleSwitcherToggale = document.querySelector(".style-switcher-toggaler");
 
         } )
     }
+
+
+    // == theme light ans dark mode ===
   
-    let number = 95934
-    console.log(number)
+  let daynight = document.querySelector(".day-night")
+
+  daynight.addEventListener('click',function(){
+    daynight.querySelector('i').classList.toggle("fa-sun")
+    daynight.querySelector('i').classList.toggle("fa-moon")
+    document.body.classList.toggle('dark')
+
+  })
+  
+
+  window.addEventListener('load', ()=>{
+    if(document.body.classList.contains('dark'))
+    daynight.querySelector("i").classList.add('fa-sun')
+else{
+    daynight.querySelector("i").classList.add('fa-moon')
+}
+
+
+  })
 
    
 
